1ddb0e071543783a305028cc087564cd2d82c716
Jose Fernández Tamame <jftamames@gmail.com>
2025-05-24 21:26:24 +0000

    feat: añade Inquiry Engine mínimo

--- cd_modules/core/inquiry_engine.py
+++ cd_modules/core/inquiry_engine.py
@@ -1,12 +1,6 @@
 from typing import List
-
 class InquiryEngine:
-    """
-    Genera una jerarquía de preguntas a partir de una cuestión inicial.
-    Implementación provisional: sólo devuelve la raíz.
-    """
     def __init__(self, root_question: str):
         self.root = root_question
-
     def generate(self) -> List[str]:
         return [self.root]
